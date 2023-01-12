834f6b8b27998e3d64091c38df01ee5a088565a6
Larsjoar96 <32615350+Larsjoar96@users.noreply.github.com>
2023-01-12 15:48:05 +0100

    Added loan mechanic

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const buttonElementAddToBank = document.getElementById("addToBank");
 let bankBalance = 400;
 let currentLoan = 0;
 let maxLoan = 0;
+let takenLoan = false;
+const interestRate = 0.1;
 const spanElementCurrentLoan = document.getElementById("currentLoan");
 const spanElementBankBalance = document.getElementById("bankBalance");
 const buttonElementTakeLoan = document.getElementById("takeLoan");
@@ -19,6 +21,7 @@ spanElementBankBalance.innerText = bankBalance;
 //Enable button functionalities
 buttonElementDoWork.addEventListener('click',DoWork);
 buttonElementAddToBank.addEventListener('click',AddToBank);
+buttonElementTakeLoan.addEventListener('click',TakeLoan);
 
 
 function DoWork()
@@ -29,8 +32,40 @@ function DoWork()
 
 function AddToBank()
 {
-    bankBalance = bankBalance + moneyEarned;
+    bankBalance = bankBalance + moneyEarned - (moneyEarned*interestRate);
+    currentLoan = currentLoan - (moneyEarned*interestRate)
     moneyEarned = 0;
+    spanElementCurrentLoan.innerText = currentLoan;
     spanElementMoneyEarned.innerText = moneyEarned;
     spanElementBankBalance.innerText = bankBalance;
+}
+
+function TakeLoan()
+{
+    if(takenLoan == false)    
+    {
+        if(bankBalance > 0) 
+        {
+            let bankInput = prompt("Enter your desired loan");
+            bankInput = parseInt(bankInput);
+            if(bankInput>bankBalance*2)
+            {
+                alert("We will not loan you more than double your money!");
+            }
+            else if(bankInput <= 0)
+            {
+                alert("That is not a valid loan number!");
+            }
+            else //Loan successfully taken
+            {
+                currentLoan = bankInput;
+                bankBalance = bankBalance + currentLoan;
+                takenLoan = true;
+                spanElementCurrentLoan.innerText = currentLoan;
+                spanElementBankBalance.innerText = bankBalance;
+            }
+        }
+        else{alert("We will not loan money to you if you do not have money!");}
+    }
+    else{alert("you already have a loan!");}
 }
